26ef93baaa5d35c02ca21bd93096762e98f41b28
opilane <opilane@HP-CZC2349HTZ>
2025-02-28 10:04:20 +0200

    .

--- Lineaarsed_programmid.py
+++ Lineaarsed_programmid.py
@@ -1,4 +1,5 @@
 from random import*
+from sunau import AUDIO_FILE_ENCODING_ADPCM_G721
 from match import*
 #ulesanne1 
 print("Hello world!","Hello world!", sep =",    ", end=")")
@@ -33,3 +34,47 @@ print(f"läbimõõt on {läbimõt}")
 N = float(input"sisesta üks pool")
 M = float(input"sisesta teine pool")
 D2 = float ((N==2)+(#==2))
+        
+   #ulesanne6
+    aeg = float(input("Mitu tundi kulus sõiduks"))
+    teepikkus=float(input("Mitu kilomeetrit sõtsid"))
+    kiirus=teepikkus/aeg
+    print("Sinu kiirus oli" + str(kiirus) + "km/h")
+
+    #ulesanne7
+  a1=randit(0,100)
+  a2=randit(0,100)
+  a3=randit(0,100)
+  a4=randit(0,100)
+  a5=randit(0,100)
+  keskmine=(a1+a2+a3+a4+a5)/5
+  print(f"Arvud olid: {a1},{a2},{a3},{a4},{a5}. Nende on keskmine on {keskmine}. ")
+
+  #ulesanne8 
+  #1
+  tekst=""
+     @..@
+    (----)
+   ( \__/ )
+   ^^ "" ^^
+   print(tekst)
+
+   #2
+print("     @..@")
+print("    (----)")
+print("   ( \__/ )")
+print(""" ^^ "" ^^""")
+
+      
+     
+     #ulesanne9
+   a=randit(1,100)
+ b=randit(1,100)
+ c=randit(1,100)
+ P=a+b+c
+ print(f"a={a}, b={b}, c={c}, P={P}")
+
+ #ulesanne 10
+ pizza=12,9
+ sõbrad=randit(2,3)
+ tip= pizza=10%+pizza
