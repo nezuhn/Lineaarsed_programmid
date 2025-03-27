f2326b45d95425106ef3cf5d65f6f18a4cad8177
opilane <opilane@HP-CZC2349HTZ>
2025-03-27 10:57:43 +0200

    26.03 tund

--- Teema5_kasutajate_fun.py
+++ Teema5_kasutajate_fun.py
@@ -30,3 +30,95 @@ def is_year_leap(aasta:int)->bool:
     else:
         v=False
     return v
+
+
+#3
+def square(külg:float)->any:
+    """
+    Funktsioon tagastab ruudu ümbermõtt, ruudu pindla ja diagonaal.
+    Kui sisend ei ole float siis tagastab "On vaja kirjutuda float".
+    :param float arv: Sisend ujukomaarvu tüübina
+    :rtype:tulemus formasdis(float) S,P,d
+    """
+    S=külg**2
+    P=4*külg
+    d=(2)**(1/2)*külg
+    return S,P,d
+
+def season(param:int)->str:
+    """Kirjeldage funktsioon
+    :param
+    :rtype:Hooajanimetus
+    """
+    if param in [1,2,12]:
+        H="Talv"
+    elif param in [3,4,5]:
+        H="Kevad"
+    elif param in [6,7,8]:
+        H="Suvi"
+    else:
+        H="Sügis"
+    return H
+
+#5
+def bank(summa:float,aastad;int)->float:
+    """funktsioon tagastab summa , mis jääb kasutaja kontole
+    on vaja kirjutada argumeendid "a" ja aastat.
+    :param
+    :param
+    :rtype:
+    """
+    for aasta in range(aastad):
+        summa*=1.1
+    return summa
+
+while True:
+    try:
+        a=float(input("Sisesta nimi:"))
+        break
+    except:
+        print("Viga! Sisesta arv")
+while True:
+    try:
+        years=float(input("Sisesta kui palju aastat: "))
+        break
+    except:
+        print("Viga! Sisesta arv")
+v=bank(summa,aastad)
+print(v)
+
+from random import *
+#6
+def is_prime(a=randit(1,10000)):
+    """
+    Funktsioon tagastab True or False
+    :param int: Sisend randit funktsioon.
+    :rtype: bool tõevärsuses formaadis tulemus
+    """
+    print(a)
+    v=True
+    for jagaja in range(2;a-1):
+        if a%jagaja==0:
+            v=False
+    return v
+
+#7
+def date(päev:int,kuu:int,aasta;int)->bool:
+    """
+    Tagastab True/False kui selline kuupäev on meie kalendris.
+    :param aasta(int)
+    :param kuu(int)
+    :param päev(int)
+    :rtype: bool tõevärsuses formaadis tulemus
+    """
+    if päev in range(1,32) and kuu [1,3,5,7,8,10,12] and aasta>0:
+        v=True
+    elif päev in range(1,30) and kuu==2 and is_year_leap(aasta):
+        v=True
+    elif päev in range(1,29) and kuu==2 and not is_year_leap(aasta):
+        v=True
+    elif päev in range(1,31) and kuu [4,6,9,11] and aasta>0:
+        v=True
+    else:
+        v=False
+    return v
--- funktsionide_kasutamine.py
+++ funktsionide_kasutamine.py
@@ -1,8 +1,23 @@
 from Teema5_kasutajate_fun import *
-
-a=float(input("Arv1: "))
-b=float(input("Arv2: "))
-t=float("Tehe: ")
+#try except!
+while True:
+    try:
+        a=float(input("Arv1: "))
+        break
+    except:
+        print("Viga! Sisesta arv")
+while True:
+    try:
+        b=float(input("Arv2: "))
+        break
+    except:
+        print("Viga! Sisesta arv")
+while True:
+    try:
+    t=input("Tehe: ")
+    break
+except:
+    print("Viga! Sisesta ((/);(*);(-);(+)")
 vastus=arithmetic(a,b,t)
 print(vastus)
 
@@ -19,3 +34,19 @@ if aasta>0:
         print(f"{aasta} on liigaasta")
     else:
         print(f"{aasta} ei ole liigaasta")
+
+
+#square() kasutamine
+#Kontroll while true ja try except...
+a=float(input("Ruudu külg= "))
+
+S,P,d=square(a)
+
+
+#7
+while True:
+päev=int(input("Sisesta päev: "))
+kuu=int(input("Sisesta kuu: "))
+aasta=int(input("Sisesta aasta: "))
+v=date(päev,kuu,aasta)
+print(v)
